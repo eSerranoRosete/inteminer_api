41fe349c3ff01a28e760f2b0853718e9bcf3b3f4
Eduardo Serrano <eserrano@Eduardos-MacBook-Air.local>
2022-06-29 12:17:26 -0500

    Added active field to User model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    first_name: String,
-    last_name: String,
-    email: String,
-    phone: String,
-    company_name: String,
-    title: String,
-    role: String,
-    group: String,
-    customerQuoter: Boolean,
-    salesQuoter: Boolean,
-    social: [{name: String, link: String}],
-    cardSettings: Object
-}); 
+  first_name: String,
+  last_name: String,
+  email: String,
+  phone: String,
+  company_name: String,
+  title: String,
+  role: String,
+  group: String,
+  customerQuoter: Boolean,
+  salesQuoter: Boolean,
+  social: [{ name: String, link: String }],
+  cardSettings: Object,
+  active: Boolean,
+});
 
-module.exports = mongoose.model('Users', userSchema);
+module.exports = mongoose.model("Users", userSchema);
